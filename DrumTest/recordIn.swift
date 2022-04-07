63cb832cac8c7df3b185eadfddba36b525351239
RockBiscuit <98774916+RockBiscuit@users.noreply.github.com>
2022-04-07 14:12:11 -0500

    Add files via upload

--- DrumTest/recordIn.swift
+++ DrumTest/recordIn.swift
@@ -16,5 +16,6 @@ struct recordIn: View {
 struct recordIn_Previews: PreviewProvider {
     static var previews: some View {
         recordIn()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
